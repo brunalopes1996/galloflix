c85da47f4f0eb6acf84e0e74d97bc83d0e6416f2
brunalopes1996 <bruuna.oliveiraa1@gmail.com>
2025-05-28 20:29:22 -0300

    Colocando os destaques

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MovieApiService } from '../../services/movie-api.service';
+import { AutoScrollDirective } from '../../directives/auto-scroll.directive';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, AutoScrollDirective],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
@@ -13,9 +14,15 @@ export class HomeComponent {
   constructor (private service: MovieApiService) {}
 
   bannerResults: any = [];
+  trendingMovieResults: any = [];
+  trendingSerieResults: any = [];
+  popularActionMovieResults: any = [];
 
   ngOnInit(): void {
     this.bannerData();
+    this.trendingMovieData();
+    this.trendingSerieData();
+    this.popularActionMovieData();
   }
 
   // Consumo do Serviço de Banner
@@ -25,4 +32,26 @@ export class HomeComponent {
       this.bannerResults = result.results;
     });
   }
+
+  // Filmes em Destaque
+  trendingMovieData() {
+    this.service.trendingMovieApiData().subscribe((result) => {
+      this.trendingMovieResults = result.results;
+    })
+  }
+
+  // Séries em Destaque
+  trendingSerieData() {
+    this.service.trendingSerieApiData().subscribe((result) => {
+      this.trendingSerieResults = result.results;
+    })
+  }
+
+  // Filmes Populares
+  popularActionMovieData() {
+    this.service.popularActionMovieApiData().subscribe((result) => {
+      this.popularActionMovieResults = result.results;
+    })
+  }
+
 }
